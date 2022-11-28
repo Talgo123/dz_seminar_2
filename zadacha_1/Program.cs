95f28fc4e5e93fd5615cccd6a2a848a714fe6b39
talgat <15talgat@mail.ru>
2022-11-28 16:46:39 +0300

    done zadacha_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine());
+int SecondDigit = number / 10 % 10;
+Console.WriteLine(number + "->" + SecondDigit);
