d7cc63da923df35be697e5155a88a7f0b0428625
talgat <15talgat@mail.ru>
2022-11-28 23:03:57 +0300

    Добавили третью задачи и альтенативное решение для второй задачи

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("это не день недели");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = int.Parse(Console.ReadLine());
+CheckingTheDayOfTheWeek (dayNumber);
