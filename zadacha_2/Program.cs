a6344b884d0c89d9bed0cc76835a2bef180d4736
talgat <15talgat@mail.ru>
2022-11-28 23:10:39 +0300

    дополнили адьтернативное решение

--- zadacha_2/Program.cs
+++ zadacha_2/Program.cs
@@ -1,22 +1,39 @@
-﻿int number = new Random().Next(1,99999);
+﻿// int number = new Random().Next(1,99999);
+// Console.WriteLine($"ввели число {number}");
+// int ThirdDigit = 0;    
+// if (number >= 100  & number < 999)
+// {
+//     ThirdDigit = number % 10;
+//     Console.WriteLine(number+ "->" + ThirdDigit);
+// }
+// if (number < 100)
+// {
+//     Console.WriteLine($"{number} -> третьей цифры нет");
+// }
+// if (number >= 1000 & number < 9999 )
+// {
+//     ThirdDigit = number /10 % 10;
+//     Console.WriteLine(number+ "->" + ThirdDigit);
+// }
+// if (number >= 10000 & number < 99999 )
+// {
+//     ThirdDigit = number /100 % 10;
+//     Console.WriteLine(number+ "->" + ThirdDigit);
+// }
+
+// Альтернативное решение
+
+
+int number = new Random().Next(1,99999);
 Console.WriteLine($"ввели число {number}");
-int ThirdDigit = 0;
-if (number >= 100  & number < 999)
-{
-    ThirdDigit = number % 10;
-    Console.WriteLine(number+ "->" + ThirdDigit);
-}
 if (number < 100)
 {
-    Console.WriteLine($"{number} -> третьей цифры нет");
+    Console.WriteLine(number + " не трехзначное число (третьей цифры нет)");
+    return;
 }
-if (number >= 1000 & number < 9999 )
+string TextNumbers = number.ToString();  //перевели  цифру в строку
+if (TextNumbers.Length > 3)  
 {
-    ThirdDigit = number /10 % 10;
-    Console.WriteLine(number+ "->" + ThirdDigit);
+     TextNumbers = TextNumbers.Substring(0, TextNumbers.Length - (TextNumbers.Length - 3)); //оставили только 3 первые цифры в строке
 }
-if (number >= 10000 & number < 99999 )
-{
-    ThirdDigit = number /100 % 10;
-    Console.WriteLine(number+ "->" + ThirdDigit);
-}
+Console.WriteLine(TextNumbers[2]); // Показываем третью цифру под индексом 2
